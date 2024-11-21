32fe0774f16e4d15a72c00d16f1ade6ed4db2bfb
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-11-20 19:37:52 -0800

    autowire fix

--- ecommerce-backend/src/main/java/com/chapman/ecommerce_backend/service/ProductService.java
+++ ecommerce-backend/src/main/java/com/chapman/ecommerce_backend/service/ProductService.java
@@ -1,50 +1,9 @@
-// package com.chapman.ecommerce_backend.service;
-
-// import java.util.List;
-// import java.util.stream.Collectors;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import com.chapman.ecommerce_backend.dto.ProductDTO;
-// import com.chapman.ecommerce_backend.entity.Product;
-// import com.chapman.ecommerce_backend.repository.ProductRepository;
-
-// @Service
-// public class ProductService {
-
-//     private final ProductRepository productRepository;
-
-//     @Autowired
-//     public ProductService(ProductRepository productRepository) {
-//         this.productRepository = productRepository;
-//     }
-
-//     public List<ProductDTO> getFeaturedProducts(int featuredProductsLimit) {
-//         List<Product> featuredProducts = productRepository.findFeaturedProducts(featuredProductsLimit);
-//         return featuredProducts.stream()
-//                 .map(this::convertToDTO)
-//                 .collect(Collectors.toList());
-//     }
-
-//     private ProductDTO convertToDTO(Product product) {
-//         return new ProductDTO(
-//                 product.getId(),
-//                 product.getName(),
-//                 product.getDescription(),
-//                 product.getPrice(),
-//                 product.getImageUrl()
-//         // Add other fields as necessary
-//         );
-//     }
-// }
-
 package com.chapman.ecommerce_backend.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.chapman.ecommerce_backend.dto.ProductDTO;
@@ -56,7 +15,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    @Autowired
+    // @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
@@ -74,8 +33,6 @@ public class ProductService {
                 product.getName(),
                 product.getDescription(),
                 product.getPrice(),
-                product.getImageUrl()
-        // Map other fields as necessary
-        );
+                product.getImageUrl());
     }
 }
