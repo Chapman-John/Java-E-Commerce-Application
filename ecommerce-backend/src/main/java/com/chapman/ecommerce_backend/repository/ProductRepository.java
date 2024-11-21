e2145b900dcf620f25a12682ec614632331c33f9
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-11-20 19:38:33 -0800

    Update ProductRepository.java

--- ecommerce-backend/src/main/java/com/chapman/ecommerce_backend/repository/ProductRepository.java
+++ ecommerce-backend/src/main/java/com/chapman/ecommerce_backend/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-// import com.chapman.ecommerce_backend.dto.ProductDTO;
 import com.chapman.ecommerce_backend.entity.Product;
 
 @Repository
