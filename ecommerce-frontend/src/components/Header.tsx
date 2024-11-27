8ce82ba6943de3f5948d24b65856dc1274847667
Chapman-John <52362698+Chapman-John@users.noreply.github.com>
2024-11-27 12:04:03 -0800

    Update Header.tsx

--- ecommerce-frontend/src/components/Header.tsx
+++ ecommerce-frontend/src/components/Header.tsx
@@ -6,8 +6,8 @@ const Header: React.FC = () => (
     <header className="sticky top-0 z-50 w-full border-b">
         <div className="container flex items-center justify-between">
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
+                <Link to="/">   Home  </Link>
+                <Link to="/products">   Products    </Link>
             </nav>
             <ModeToggle />
         </div>
